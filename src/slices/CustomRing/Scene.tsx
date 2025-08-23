332dbfa5078c3257161b7a5759e673afc7218a95
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-08-23 11:05:33 +0700

    wood , metal

--- src/slices/CustomRing/Scene.tsx
+++ src/slices/CustomRing/Scene.tsx
@@ -1,11 +1,9 @@
 "use client";
 
-import { Environment } from "@react-three/drei";
 import { useRef } from "react";
 import { Group } from "three";
 import { useFrame } from "@react-three/fiber";
 import CustomRingModel from "@/Components/CustomRingModel";
-import EngravingText from "@/Components/EngravingText";
 
 type Props = {
   position?: [number, number, number];
@@ -30,12 +28,6 @@ export default function SceneCustomRing({
   return (
     <group ref={ringRef} position={position} rotation={rotation} scale={scale}>
       <CustomRingModel scale={6} />
-      <EngravingText
-        text="MINH ANH"
-        position={[0, 0.3, 1]}
-        rotation={[0, Math.PI / 2, 0]}
-      />
-      <Environment files={"/HDR/lobby.hdr"} environmentIntensity={3} />
     </group>
   );
 }
